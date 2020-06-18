22753eb9e19b682f9ff9ff345910ec3ea566e164
Khoirul Umam <khoirul.umam35@gmail.com>
2020-06-18 13:53:23 +0700

    Add title & profpict

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ const Home = () => (
 	<MainLayout>
 		<div className="container">
 			<div className="row">
+				<div className="col-sm"><h1 className="display-1">Mas Umam</h1></div>
+				<div className="col-sm"><img src="/images/khoirul-umam.jpg" className="img-thumbnail" width="200px" /></div>
+			</div>
+			<div className="row">
 				<div className="col-sm">Halo nama saya Umam</div>
 				<div className="col-sm">Saya dosen di STIKOM PGRI Banyuwangi</div>
 				<div className="col-sm">Bidang saya di Teknik Informatika</div>
